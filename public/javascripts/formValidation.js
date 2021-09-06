ceb4d99e93663ff835d573682fb9f209f5ac8090
namank63 <namank63@gmail.com>
2021-09-06 13:04:10 +0530

    Auth Pages updated and Captacha added!!

--- public/javascripts/formValidation.js
+++ public/javascripts/formValidation.js
@@ -1,12 +1,24 @@
+var gencaptcha = document.querySelector("#gencaptcha");
+var newCaptcha = generateCaptcha();
+gencaptcha.innerHTML = newCaptcha;
+var refreshFunctionCheck = false;
+
+document.getElementById("refreshButton").addEventListener("click", function() {
+    refreshFunctionCheck = true;
+    newCaptcha = generateCaptcha();
+    gencaptcha.innerHTML = newCaptcha;
+    console.log("Working!!!!");
+});
+
 function registerFormValidation() {
     var username = document.registration.username;
     var createpassword = document.registration.createpassword;
     var confirmpassword = document.registration.confirmpassword;
     var uemail = document.registration.email;
 
-    if(username_validation(username, 5, 10)) {
+    if(username_validation(username, 5, 16)) {
         if(email_validation(uemail)) {
-            if(password_validation(createpassword, confirmpassword, 5, 10)) {
+            if(password_validation(createpassword, confirmpassword, 5, 16)) {
                 return true;
             }
         }
@@ -14,13 +26,29 @@ function registerFormValidation() {
     return false;
 }
 
+function generateCaptcha() {
+    var chr1 = Math.ceil(Math.random() * 10) + '';  
+    var chr2 = Math.ceil(Math.random() * 10) + '';  
+    var chr3 = Math.ceil(Math.random() * 10) + '';  
+    var str = new Array(4).join().replace(/(.|$)/g, function () { return ((Math.random() * 36) | 0).toString(36)[Math.random() < .5 ? "toString" : "toUpperCase"](); });  
+    var captchaCode = str + chr1 + '' + chr2 + '' + chr3;
+    return captchaCode;
+}
+
 function loginFormValidation() {
     var username = document.login.username;
     var password = document.login.password;
+    var entercaptcha = document.login.entercaptcha;
+    var gencaptcha = document.querySelector("#gencaptcha");
 
-    if(username_validation(username, 5, 10)) {
+    if(username_validation(username, 5, 16)) {
         if(password_validation(password, password)) {
-            return true;
+            if(gencaptcha.innerHTML === entercaptcha.value) {
+                return true;
+            } else {
+                if(refreshFunctionCheck == false)
+                    alert("Enter correct Captcha!!");
+            }
         }
     }
     return false;
@@ -48,7 +76,7 @@ function email_validation(uemail) {
 }
 
 function password_validation(createpassword, confirmpassword) {
-    var passwordFormat = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{5,10}$/;
+    var passwordFormat = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{5,16}$/;
     var passid_len = createpassword.value.length;
     if(createpassword.value.match(passwordFormat)) {
         if(createpassword.value == confirmpassword.value) {
@@ -59,8 +87,9 @@ function password_validation(createpassword, confirmpassword) {
             return false;
         }
     } else {
-        alert("The length of password must be between 5 to 10 and it must contain at least a letter, a number and a special character");
+        alert("The length of password must be between 5 to 16 and it must contain at least a letter, a number and a special character");
         createpassword.focus();
         return false;
     }
 }
+
